50185e17a97a801683539847d696df0fa220d20b
emmadema <emma.demango@gmail.com>
2018-02-13 23:52:09 -0700

    adding messages service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,26 +9,22 @@ import { HeroService } from '../hero.service';
 })
 
 export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
 
   heroes: Hero[];
 
-	selectedHero: Hero;
-
- 
-
   constructor(private heroService: HeroService) { }
 
-   onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+    ngOnInit() {
+      this.getHeroes();
+    }
 
-  getHeroes(): void {
-    this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
-  }
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
 
-  ngOnInit() {
-    this.getHeroes();
-  }
-
+    getHeroes(): void {
+      this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+    }
 }
