784686d47167b9f1a65f6fbdde8eeac919fb5708
emmadema <emma.demango@gmail.com>
2018-02-14 00:44:44 -0700

    adding http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 
 import { AppComponent } from './app.component';
