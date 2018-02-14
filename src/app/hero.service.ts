d8f7e171310295f7a1828c763ee295c03ab7be1c
emmadema <emma.demango@gmail.com>
2018-02-14 09:39:37 -0700

    working on http request

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero';
-import  { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 
 @Injectable()
 export class HeroService {
 	
-  constructor(private messageService: MessageService) { }
+  	constructor(
+  		private http: HttpClient,
+  		private messageService: MessageService
+  	) { }
+
+  	private log
 
 	getHeroes(): Observable<Hero[]> {
 		this.messageService.add('HeroService: fetched heroes');
